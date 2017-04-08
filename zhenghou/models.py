3de27a1a142e98c0ee310ea472fa90e77cf2b912
huozipan <greyzp@gmail.com>
2017-04-08 08:17:40 +0800

    new models

--- zhenghou/models.py
+++ zhenghou/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+  name = models.CharField(max_length=100)
+
+class Descript(models.Model):
+  name = models.CharField(max_length=100)
+
+class GroundFact(models.Model):
+  name = models.CharField(max_length=100)
+  position = models.ForeignKey('Position', on_delete=models.SET_NULL)
+  descript = models.ForeignKey('Descript', on_delete=models.SET_NULL)
+  level    = models.IntergerField(default=1)
+
+class ZhengHou(models.Model):
+  name      = models.CharField(max_length=100)
+  zhenghous = models.ManyToManyField(ZhengHou)
+  level     = models.IntergerField(default=1)
+  gfs       = models.ManyToManyField(GroundFact)
+
+class Synonym(models.Model):
+  name      = models.CharField(max_length=100)
+  syno      = models.CharField(max_length=100)
+
+
