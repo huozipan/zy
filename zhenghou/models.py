453634392a858d5c5e3d67faf21fe7c84779b5c4
huozipan <greyzp@gmail.com>
2017-04-08 15:51:10 +0800

    first webpage

--- zhenghou/models.py
+++ zhenghou/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Position(models.Model):
-  name = models.CharField(max_length=100)
+  name = models.CharField(max_length=100, unique=True)
 
 class Descript(models.Model):
-  name = models.CharField(max_length=100)
+  name = models.CharField(max_length=100, unique=True)
 
 class GroundFact(models.Model):
-  name = models.CharField(max_length=100)
-  position = models.ForeignKey('Position', on_delete=models.SET_NULL)
-  descript = models.ForeignKey('Descript', on_delete=models.SET_NULL)
-  level    = models.IntergerField(default=1)
+  name = models.CharField(max_length=100, unique=True)
+  position = models.ForeignKey('Position', on_delete=models.PROTECT)
+  descript = models.ForeignKey('Descript', on_delete=models.PROTECT)
+  level    = models.IntegerField(default=1)
 
 class ZhengHou(models.Model):
-  name      = models.CharField(max_length=100)
-  zhenghous = models.ManyToManyField(ZhengHou)
-  level     = models.IntergerField(default=1)
+  name      = models.CharField(max_length=100, unique=True)
+  zhenghous = models.ManyToManyField('self')
+  level     = models.IntegerField(default=1)
   gfs       = models.ManyToManyField(GroundFact)
 
 class Synonym(models.Model):
-  name      = models.CharField(max_length=100)
+  name      = models.CharField(max_length=100, unique=True)
   syno      = models.CharField(max_length=100)
 
 
--- zhenghou/views.py
+++ zhenghou/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
 # Create your views here.
+import jieba
+import jieba.posseg as pseg
+
+from .models import GroundFact
+
+g_dicts = {}
+
+for e in GroundFact.objects.all():
+  jieba.add_word(e.name)
+  g_dicts[e.name] = ':'+e.name
+#
+#for e in ZhengHou.objects.all():
+#  jieba.add_word(e.name)
+#  g_dicts[e.name] = ':'+e.name
+
+
+from .forms import ZhengHouForm
+def zhenghou(request):
+  if request.method == 'POST':
+    form = ZhengHouForm(request.POST)
+    if form.is_valid():
+      name = form.cleaned_data['zh_name']
+      message = form.cleaned_data['zh_message']
+      result = form.cleaned_data['zh_result']
+      i = form.cleaned_data['zh_is']
+
+      words = pseg.cut(message)
+      tmp_str = ''
+      for word,flag in words:
+        if flag == 'x':
+          continue
+        if word in g_dicts:
+          tmp_str = ("%s:%s\n" %(tmp_str, word))
+        else:
+          tmp_str = ("%s%s\n" %(tmp_str, word))
+
+      if i==False:
+        newDict = {
+            'zh_name':name,
+            'zh_message': message,
+            'zh_result': tmp_str
+            }
+        newForm = ZhengHouForm(newDict)
+        print(newForm)
+        return render(request, 'zhenghou.html', {'form': newForm})
+    pass
+  else:
+    form = ZhengHouForm()
+
+  return render(request, 'zhenghou.html', {'form': form})
