c3a8a520397202894193895c615dda6d75381c4d
huozipan <zphuo@hotmail.com>
2017-05-15 15:34:14 +0800

    add rawInputView urlconf

--- zhenghou/urls.py
+++ zhenghou/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.zhenghou, name='zhenghou')
+    url(r'^$', views.zhenghou, name='zhenghou'),
+    url(r'^rawInput$', views.RawInput.as_view, name="rawinput"),
 ]
